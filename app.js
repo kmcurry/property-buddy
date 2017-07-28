3d417009600c7b0ead9bb639a9f4b0230a09e2eb
Kevin Curry <kmcurry@gmail.com>
2017-07-27 21:29:58 -0400

    configure port

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
