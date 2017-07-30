bd89040a3f760ef2beedc8d8d082eb912b9d30a0
Kevin Curry <kmcurry@gmail.com>
2017-07-30 12:14:11 -0400

    put bower in correct place

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
   res.render('index');
