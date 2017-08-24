29422c84cddfbb5b31b32e36cc37185a56b534ff
Kevin Curry <kmcurry@gmail.com>
2017-08-23 21:11:01 -0400

    start North Carolina and add G-Analytics

---
+++ sources/NorthCarolina/Raleigh.js
@@ -0,0 +1,38 @@
+var Raleigh = {
+
+  boundary: "",
+  AICUZ: "",
+  FIRM: "",
+  hospitals: "",
+  hydrants: "",
+  libraries: "",
+
+  neighborhoods: "",
+
+  parks: "",
+  recCenters: "",
+  waterBodies: "",
+
+  schools: {
+    elementary: "",
+    middle: "",
+    high: ""
+  },
+
+  fire: {
+    incidents: "https://data.raleighnc.gov/resource/nqdf-wvzj.json"
+  }
+
+  emergency: {
+    calls: ""
+  },
+
+  police: {
+      calls: "",
+      incidents: ""
+  }
+
+};
+
+
+module.exports = Chesapeake;
