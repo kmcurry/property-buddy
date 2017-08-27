4cd7f896abd217eaed65a1dd50fa34428e09db22
Kevin Curry <kmcurry@gmail.com>
2017-08-27 15:35:31 -0400

    add Richmond

---
+++ locations/Virginia/Richmond.js
@@ -0,0 +1,49 @@
+var Richmond = {
+
+  boundary: "",
+  neighborhoods: "https://data.richmondgov.com/resource/7juf-nwis.json",
+  AICUZ: "",
+  FIRM: "",
+
+  fire: {
+    calls: "",
+    districts: "",
+    hydrants: {
+      public: "",
+      private: ""
+    },
+    incidents: ""
+  },
+
+  medical: {
+    emergency: {
+      calls: ""
+    },
+    facilities: "",
+    hospitals: ""
+  },
+
+  police: {
+    calls: "",
+    incidents: ""
+  },
+
+  recreation: {
+    centers: "",
+    greenways: "",
+    libraries: "",
+    parks: "",
+    trails: "",
+    water: ""
+  },
+
+  schools: {
+    all: "",
+    elementary: "",
+    high: "",
+    middle: "",
+    pre: ""
+  }
+};
+
+module.exports = Richmond;
