2eb44f4590359a01399e6a19c5507f443af91f9e
Kevin Curry <kmcurry@gmail.com>
2017-08-29 11:16:41 -0400

    add Charlottesville

---
+++ locations/Virginia/Charlottesville.js
@@ -0,0 +1,49 @@
+var LocationName = {
+
+  boundary: "https://opendata.arcgis.com/datasets/43253262b4da436bbac25d8cdb2f043b_44.geojson",
+  neighborhoods: "",
+  AICUZ: "",
+  FIRM: "",
+
+  fire: {
+    calls: "",
+    districts: "",
+    hydrants: {
+      public: "",
+      private: ""
+    },
+    incidents: ""
+  },
+
+  medical: {
+    emergency: {
+      calls: ""
+    },
+    facilities: "",
+    hospitals: ""
+  },
+
+  police: {
+    calls: "",
+    incidents: "https://opendata.arcgis.com/datasets/6d17dc31b79943ee9214d048ec46be53_16.geojson"
+  },
+
+  recreation: {
+    centers: "",
+    greenways: "",
+    libraries: "",
+    parks: "https://opendata.arcgis.com/datasets/a13bdf43fff04168b724a64f7dca234d_19.geojson",
+    trails: "https://opendata.arcgis.com/datasets/5d79c6a3e6d44a758063c59c8e00b2dc_18.geojson",
+    water: ""
+  },
+
+  schools: {
+    all: "",
+    elementary: "https://opendata.arcgis.com/datasets/6d17dc31b79943ee9214d048ec46be53_16.geojson",
+    high: "",
+    middle: "",
+    pre: ""
+  }
+};
+
+module.exports = LocationName;
