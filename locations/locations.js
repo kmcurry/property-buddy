2223490edb7bad85d421750b49018b8a6db68056
Kevin Curry <kmcurry@gmail.com>
2018-09-13 11:50:45 -0400

    add Portsmouth and Hampton

--- locations/locations.js
+++ locations/locations.js
@@ -6,7 +6,9 @@ locations.NorthCarolina.Raleigh  = require('./NorthCarolina/Raleigh');
 locations.Virginia = {};
 locations.Virginia.Chesapeake    = require('./Virginia/Chesapeake');
 locations.Virginia.FallsChurch   = require('./Virginia/FallsChurch');
+locations.Virginia.Hampton       = require('./Virginia/Hampton');
 locations.Virginia.Norfolk       = require('./Virginia/Norfolk');
+locations.Virginia.Portsmouth    = require('./Virginia/Portsmouth');
 locations.Virginia.VirginiaBeach = require('./Virginia/VirginiaBeach');
 
 module.exports = locations;
