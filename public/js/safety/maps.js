48babe1bd8f4003ecdaae0c9c42d1ac850587270
Kevin Curry <kmcurry@gmail.com>
2019-01-25 16:42:36 -0500

    make choropleth

--- public/js/safety/maps.js
+++ public/js/safety/maps.js
@@ -3,19 +3,18 @@ var map = L.map('crimeMap').setView([36.78, -76.00], 10);
 
 
 function getColor(d) {
-    return d > 1000 ? '#800026' :
-        d > 500 ? '#BD0026' :
-        d > 200 ? '#E31A1C' :
-        d > 100 ? '#FC4E2A' :
-        d > 50 ? '#FD8D3C' :
-        d > 20 ? '#FEB24C' :
-        d > 10 ? '#FED976' :
-        '#FFEDA0';
+    
+    return  d > 80 ? '#993404' :
+        d > 50 ? '#d95f0e' :
+        d > 20 ? '#fe9929' :
+        d > 10 ? '#fed98e' :
+        '#ffffd4';
 }
 
 function style(feature) {
+    console.log(feature.properties.incidents.length);
     return {
-        fillColor: '#FEB24C',
+        fillColor: getColor(feature.properties.incidents.length),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -28,18 +27,82 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
     id: 'mapbox.light'
 }).addTo(map);
 
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 80],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Police Incidents by Patrol Zone</h4>' +  (props ?
+        '<b>' + props.BEAT + '</b><br />' + props.incidents.length + ' incidents'
+        : '');
+};
+
+info.addTo(map);
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function onEachFeature(feature, layer) {
+    console.log("nnnnnn")
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+}
+
 var patrol_zone_boundary = new L.geoJson();
 patrol_zone_boundary.addTo(map, {
-    style: style
+    onEachFeature: onEachFeature
 });
 
 $.ajax({
     dataType: "json",
     url: "https://gis.data.vbgov.com/datasets/82ada480c5344220b2788154955ce5f0_9.geojson",
     success: function (data) {
-        $(data.features).each(function (key, data) {
-            patrol_zone_boundary.addData(data);
-        });
 
         var locations = $("#locations").val();
         //console.log(locations);
@@ -51,7 +114,15 @@ $.ajax({
 
         getCountWithinDays(DataDirectory.police.incidents, [-76.00, 36.78], 40, 30, "police-incidents").then(function (incidents) {
             if (incidents) {
-                console.log(incidents);
+                $(data.features).each(function (key, data) {
+                    //console.log(data.properties.BEAT);
+                    var incidentsInBeat = $(incidents).filter(function(index) {
+                        return incidents[index].zone_id == data.properties.BEAT;
+                    })
+                    data.properties.incidents = incidentsInBeat;
+                    patrol_zone_boundary.addData(data);
+                });
+                patrol_zone_boundary.setStyle(style);
             }
         });
     }
