52f793805df694fc13dc665d4aa8957e84343926
Kevin Curry <kmcurry@gmail.com>
2019-02-21 12:24:46 -0500

    start these

---
+++ public/js/prices/oysters.js
@@ -0,0 +1,26 @@
+var Oysters = {
+    'Seaside': {
+        
+    },
+    'Upper Bay Eastern Shore': {
+        
+    },
+    'Lower Bay Eastern Shore': {
+
+    },
+    'Upper Bay Western Shore': {
+
+    },
+    'Middle Bay Western Shore': {
+
+    },
+    'Lower Bay Western Shore': {
+
+    },
+    'Tidewater': {
+
+    },
+    'Tangier/Middle Chesapeake Bay': {
+
+    }
+};
---
+++ public/js/prices/products.js
@@ -0,0 +1,103 @@
+var Products = {
+
+    Seafood: {
+
+        Crabs,
+        Clams,
+        Oysters: [
+            'Seaside',
+            'Upper Bay Eastern Shore',
+            'Lower Bay Eastern Shore',
+            'Upper Bay Western Shore',
+            'Middle Bay Western Shore',
+            'Lower Bay Western Shore',
+            'Tidewater',
+            'Tangier/Middle Chesapeake Bay'
+        ],
+        Scallops,
+        Shrimp,
+        Bluefish,
+        Flounder,
+        Rockfish,
+        Trout,
+        Spot,
+        Tuna,
+
+    },
+
+
+    Meat: {
+
+        Beef,
+        Pork,
+        Poultry,
+
+        Bison,
+        Venison,
+
+    },
+
+
+    Dairy: {
+
+        Eggs,
+        Milk,
+
+        Butter,
+        Cheese,
+        Yogurt,
+
+    },
+
+
+
+    Fruit: {
+
+        Apples,
+        Cherries,
+        Peaches,
+
+        Blackberries,
+        Blueberries,
+        Raspberries,
+        Strawberries,
+
+    },
+
+
+    Vegetable: {
+
+        Asparagus,
+        Peanuts,
+
+    },
+
+
+
+    Coffee: {
+
+        Fathom,
+        Jolly Roasters,
+        Lynnhaven,
+        Three Ships,
+
+    },
+
+
+    Beer: {
+
+        Back Bay,
+        Commonwealth,
+        Deadline,
+        Pleasure House,
+        Reaver Beach,
+        Young Veterans,
+        Wasserhund,
+
+        Big Ugly,
+        Bold Mariner,
+        Coelacanth,
+        O 'Connor,
+        Smartmouth,
+    }
+};
